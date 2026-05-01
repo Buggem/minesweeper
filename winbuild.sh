684ea0746b5f434a2f2d8b04ae9973a682eaeb43
Max Parry <66670928+Buggem@users.noreply.github.com>
2026-05-01 22:21:16 +1000

    Windows build - add script and icon

--- winbuild.sh
+++ winbuild.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-CC=x86_64-w64-mingw32-gcc CFLAGS="-lmingw32 -lSDL2_image -lSDL2main -lSDL2 -DEMBEDDEDPNG -lm -mwindows" ./build.sh 
-
+x86_64-w64-mingw32-windres mines.rc -O coff -o mines.res
+CC=x86_64-w64-mingw32-gcc CFLAGS="mines.res -lmingw32 -lSDL2_image -lSDL2main -lSDL2 -DEMBEDDEDPNG -lm -mwindows" ./build.sh 
+rm -f mines.res
