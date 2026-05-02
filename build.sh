b9154ec747be15716e8d724270def88b02899a75
Max Parry <66670928+Buggem@users.noreply.github.com>
2026-05-02 10:52:44 +1000

    Optimizations

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 CC="${CC:-gcc}"
-CFLAGS="${CFLAGS:--lSDL2 -lSDL2_image -DEMBEDDEDPNG -lm}"
+CFLAGS="${CFLAGS:--lSDL2 -lSDL2_image -DEMBEDDEDPNG -lm -O2}"
 xxd -i mines.png > src/mines_png.h
 xxd -i icon.png > src/icon_png.h
 $CC src/*.c $CFLAGS -o minesweeper
--- winbuild.sh
+++ winbuild.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 x86_64-w64-mingw32-windres mines.rc -O coff -o mines.res
-CC=x86_64-w64-mingw32-gcc CFLAGS="mines.res -lmingw32 -lSDL2_image -lSDL2main -lSDL2 -DEMBEDDEDPNG -lm -mwindows" ./build.sh 
+CC=x86_64-w64-mingw32-gcc CFLAGS="mines.res -lmingw32 -lSDL2_image -lSDL2main -lSDL2 -DEMBEDDEDPNG -lm -mwindows -O2" ./build.sh
 rm -f mines.res
