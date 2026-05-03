f2cd39cc8d3d62d42a15bc6b1e7ed3ce756d633d
Buggem's Fedora PC <66670928+Buggem@users.noreply.github.com>
2026-05-03 12:45:58 +1000

    Assets, great great assets

--- build.sh
+++ build.sh
@@ -2,8 +2,10 @@
 
 CC="${CC:-gcc}"
 CFLAGS="${CFLAGS:--lSDL2 -lSDL2_image -DEMBEDDEDPNG -lm -O2}"
-xxd -i mines.png > src/mines_png.h
-xxd -i icon.png > src/icon_png.h
+cd assets
+xxd -i mines.png > ../src/mines_png.h
+xxd -i icon.png > ../src/icon_png.h
+cd ..
 $CC src/*.c $CFLAGS -o minesweeper
 rm -f src/mines_png.h
 rm -f src/icon_png.h
